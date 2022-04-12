37e2566e6de1a0a6d1cce9f549835bbd57b58489
niha8515 <niha8515@yahoo.com>
2022-04-12 16:00:35 +0430

    add fun checkFields in EditProfileFragment.kt

--- app/src/main/java/com/example/accountmanager/profile/EditProfileFragment.kt
+++ app/src/main/java/com/example/accountmanager/profile/EditProfileFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.core.text.isDigitsOnly
 import androidx.navigation.fragment.findNavController
 import com.example.accountmanager.R
 import com.example.accountmanager.databinding.FragmentEditProfileBinding
@@ -28,8 +28,35 @@ class EditProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
          binding.registerBtn.setOnClickListener {
-             Toast.makeText(activity, "salam dada", Toast.LENGTH_SHORT).show()
-             findNavController().navigate(R.id.action_editProfileFragment_to_showProfileFragment)
+             if (checkFields()) {
+                 findNavController().navigate(R.id.action_editProfileFragment_to_showProfileFragment)
+             }
          }
+    }
+
+    private fun checkFields(): Boolean {
+        var result = false
+        if (binding.nameEdt.text.isNullOrBlank()){
+            binding.nameEdt.error = getString(R.string.messageError)
+        }
+        else if (binding.fatherNameEdt.text.isNullOrBlank()){
+            binding.fatherNameEdt.error = getString(R.string.messageError)
+        }
+        else if (binding.postcodeEdt.text.isNullOrBlank()) {
+            binding.postcodeEdt.error = getString(R.string.messageError)
+        }else if (!binding.postcodeEdt.text?.isDigitsOnly()!!
+            || binding.postcodeEdt.text!!.length != 9){
+            binding.postcodeEdt.error = getString(R.string.incorrectMessageError)
+        }
+        else if (binding.phoneNumberEdt.text.isNullOrBlank()){
+            binding.phoneNumberEdt.error = getString(R.string.messageError)
+        }else if (!binding.phoneNumberEdt.text?.isDigitsOnly()!!
+            || binding.phoneNumberEdt.text!!.length != 11){
+            binding.phoneNumberEdt.error = getString(R.string.incorrectMessageError)
+        }
+        else{
+            result = true
+        }
+        return result
     }
 }
