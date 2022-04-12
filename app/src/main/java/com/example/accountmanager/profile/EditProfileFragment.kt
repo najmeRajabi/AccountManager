47e33ef0f32ed35d9816d7e90d49270a6ae44e5b
niha8515 <niha8515@yahoo.com>
2022-04-12 15:26:20 +0430

    add click listener btn register in EditProfileFragment.kt

--- app/src/main/java/com/example/accountmanager/profile/EditProfileFragment.kt
+++ app/src/main/java/com/example/accountmanager/profile/EditProfileFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.accountmanager.R
+import com.example.accountmanager.databinding.FragmentEditProfileBinding
 
 class EditProfileFragment : Fragment() {
+    lateinit var binding: FragmentEditProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +21,15 @@ class EditProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+        binding = FragmentEditProfileBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+         binding.registerBtn.setOnClickListener {
+             Toast.makeText(activity, "salam dada", Toast.LENGTH_SHORT).show()
+             findNavController().navigate(R.id.action_editProfileFragment_to_showProfileFragment)
+         }
     }
 }
