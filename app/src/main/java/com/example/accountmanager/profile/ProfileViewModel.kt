c8adf59f05df098e4168f74cfc774ec3c1e44ecc
niha8515 <niha8515@yahoo.com>
2022-04-12 15:27:05 +0430

    add ProfileViewModel.kt

---
+++ app/src/main/java/com/example/accountmanager/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.accountmanager.profile
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class ProfileViewModel(app:Application):AndroidViewModel(app) {
+}
