ee07fd842313a29c7b4ed16964dc963233b14535
niha8515 <niha8515@yahoo.com>
2022-04-13 19:20:08 +0430

    fix bug

--- app/src/main/java/com/example/accountmanager/database/Account.kt
+++ app/src/main/java/com/example/accountmanager/database/Account.kt
@@ -1,11 +1,12 @@
 package com.example.accountmanager.database
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
 data class Account(
     val AccountType:String,
-    val cartNumber: String,
+    @PrimaryKey val cartNumber: String,
     val stock: String //موجودی
 ) {
 }
