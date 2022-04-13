aa87d79e534f8f7ffb1592d993bbf45a1d7f0a2b
niha8515 <niha8515@yahoo.com>
2022-04-14 00:37:15 +0430

    add fun for deleteAll button

--- app/src/main/java/com/example/accountmanager/MainActivity.kt
+++ app/src/main/java/com/example/accountmanager/MainActivity.kt
@@ -3,15 +3,21 @@ package com.example.accountmanager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import android.widget.Toolbar
+import androidx.activity.viewModels
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
 import androidx.navigation.ui.AppBarConfiguration
+import com.example.accountmanager.account.AccountViewModel
 import com.example.accountmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
+    val vModel :AccountViewModel by viewModels()
     private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +41,21 @@ class MainActivity : AppCompatActivity() {
         toolbar.setupWithNavController(navController,appBarConfiguration)
         binding.navView.setupWithNavController(navController)
         setSupportActionBar(toolbar)
+
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId){
+
+                R.id.deleteAllAccountBtn -> {
+                    //todo dialog sure
+                   vModel.deleteAll()
+                    Toast.makeText(this,"deleted",Toast.LENGTH_SHORT).show()
+                    true
+                }
+            }
+            NavigationUI.onNavDestinationSelected(it, navController)
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
     }
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
