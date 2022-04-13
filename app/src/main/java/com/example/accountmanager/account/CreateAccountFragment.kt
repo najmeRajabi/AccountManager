ce41997a8c7381bf1a4be09bf3d2533bbb7d20bd
niha8515 <niha8515@yahoo.com>
2022-04-13 10:39:43 +0430

    add adapter for Account type in CreateAccountFragment.kt

--- app/src/main/java/com/example/accountmanager/account/CreateAccountFragment.kt
+++ app/src/main/java/com/example/accountmanager/account/CreateAccountFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import com.example.accountmanager.R
+import com.example.accountmanager.databinding.FragmentCreateAccountBinding
 
 class CreateAccountFragment : Fragment() {
+    lateinit var binding: FragmentCreateAccountBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +23,14 @@ class CreateAccountFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_account, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val items = listOf("قرض الحسنه", "جاری", "بلند مدت", "کوتاه مدت")
+        val adapter = ArrayAdapter(requireContext(), R.layout.list_item_account_type, items)
+        (binding.accountTypeEdt as? AutoCompleteTextView)?.setAdapter(adapter)
     }
 
 }
