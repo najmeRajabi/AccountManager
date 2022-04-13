c40b7233a3ead5af2de526de298d0530c2b09115
niha8515 <niha8515@yahoo.com>
2022-04-13 14:59:45 +0430

    add AccountViewModel.kt

---
+++ app/src/main/java/com/example/accountmanager/account/AccountViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.accountmanager.account
+
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
+import com.example.accountmanager.database.Account
+import com.example.accountmanager.database.AccountDao
+import com.example.accountmanager.database.AppDatabase
+
+class AccountViewModel(app:Application):AndroidViewModel(app) {
+
+
+
+}
--- app/src/main/java/com/example/accountmanager/database/AccountRepository.kt
+++ app/src/main/java/com/example/accountmanager/database/AccountRepository.kt
@@ -1,4 +1,41 @@
 package com.example.accountmanager.database
 
+import android.content.Context
+
 object AccountRepository {
+
+    var db : AppDatabase? = null
+    var accountDao :AccountDao? = null
+
+    fun initDB(context : Context){
+        db = AppDatabase.getAppDatabase(context)
+
+        accountDao = db?.accountDao()
+
+        addTestData()
+    }
+
+    private fun addTestData() {
+        accountDao?.insertAll(
+            Account("1","1","1"),
+            Account("2","2","2"),
+
+            )
+    }
+
+    fun getQuestions() : List<Account>{
+        return db!!.accountDao().getAllAccount()
+    }
+
+    fun deleteAll(){
+        db?.accountDao()?.deleteAll()
+    }
+
+    fun insertAccount(account: Account){
+        db?.accountDao()?.insertAll(account)
+    }
+
+    fun updateAccount(account: Account){
+        db?.accountDao()?.update(account)
+    }
 }
