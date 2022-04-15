1b8333ff232acb3e1c0ba9466ab48fdb3b71e9df
niha8515 <niha8515@yahoo.com>
2022-04-15 17:25:05 +0430

    add enable for buttons next , back in view model

--- app/src/main/java/com/example/accountmanager/account/AccountViewModel.kt
+++ app/src/main/java/com/example/accountmanager/account/AccountViewModel.kt
@@ -1,21 +1,37 @@
 package com.example.accountmanager.account
 
 import android.app.Application
+import android.content.Context
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.accountmanager.database.Account
 import com.example.accountmanager.database.AccountRepository
 
 class AccountViewModel(app:Application):AndroidViewModel(app) {
 
 
+    var counter = 1
+    val context = app.applicationContext
+    val numberAccount = MutableLiveData<Int>(0)
+//    val size = MutableLiveData<Int>(1)
+
     private lateinit var  accountList : List<Account>
-    lateinit var account  : LiveData<Account>
+    var account  : LiveData<Account>?
+    var enableNextBtn = MutableLiveData(true)
+    var enableBackBtn = MutableLiveData(false)
+
+    lateinit var AllAccountLiveData:LiveData<List<Account>>
+
     init{
         AccountRepository.initDB(app.applicationContext)
         accountList = AccountRepository.getAccounts()
-        account= AccountRepository.getAccountLiveData()
+//        size.value= accountList.size
+        account= AccountRepository.getAccountLiveData(numberAccount)
 //        account.value = accountList[0]
+
+        AllAccountLiveData = AccountRepository.getAllAccountLiveData()
     }
 
     fun insert(account: Account){
@@ -27,7 +43,33 @@ class AccountViewModel(app:Application):AndroidViewModel(app) {
     }
 
     fun deleteAll(){
-        AccountRepository.deleteAll(accountList)
+
+            AccountRepository.deleteAll(AccountRepository.getAccounts())
+
+    }
+
+    fun nextAccount() {
+        counter++
+        AllAccountLiveData = AccountRepository.getAllAccountLiveData()
+        enableBackBtn.value=true
+        numberAccount.value = numberAccount.value?.plus(1)
+        account = AccountRepository.getAccountLiveData(numberAccount)
+        Toast.makeText(context,"next",Toast.LENGTH_SHORT).show()
+        if (numberAccount.value == accountList.size){
+            enableNextBtn.value = false
+        }
+
+    }
+
+    fun prevAccount() {
+        counter--
+        enableNextBtn.value = true
+        numberAccount.value = numberAccount.value?.minus(1)
+        account = AccountRepository.getAccountLiveData(numberAccount)
+        Toast.makeText(context,"back",Toast.LENGTH_SHORT).show()
+        if (numberAccount.value == 1){
+            enableBackBtn.value = false
+        }
     }
 
 //    fun update(account: Account){
