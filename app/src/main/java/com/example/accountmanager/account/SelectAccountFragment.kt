c9cb7d0487e84887bc31bef2196f5617cafaad7a
niha8515 <niha8515@yahoo.com>
2022-04-18 14:51:18 +0430

    add click listener for findAccount button in SelectAccountFragment.kt

--- app/src/main/java/com/example/accountmanager/account/SelectAccountFragment.kt
+++ app/src/main/java/com/example/accountmanager/account/SelectAccountFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import com.example.accountmanager.R
+import com.example.accountmanager.databinding.FragmentSelectAccountBinding
 
 class SelectAccountFragment : Fragment() {
 
+    lateinit var binding: FragmentSelectAccountBinding
+    val vModel :AccountViewModel by activityViewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,7 +23,40 @@ class SelectAccountFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_select_account, container, false)
+        binding = FragmentSelectAccountBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.chooseAccountBtn.setOnClickListener {
+            if (checkCartNumber()){
+                fillField()
+
+            }
+        }
+    }
+
+    private fun fillField() {
+        val account = vModel.getAccountByCartNumber(binding.accountNumberEdtChoose.text.toString())
+        if (account == null){
+            Toast.makeText(requireContext(),"چنین حسابی پیدا نشد!",Toast.LENGTH_SHORT).show()
+        }else{
+            binding.llChooseAccountShow.visibility = View.VISIBLE
+            binding.txvAccountNumberChoose.text = account.cartNumber
+            binding.txvAccountStockChoose.text = account.stock
+            binding.txvAccountTypeChoose.text = account.AccountType
+        }
+    }
+
+    private fun checkCartNumber(): Boolean {
+        return if (binding.accountNumberEdtChoose.text.isNullOrBlank()
+            || binding.accountNumberEdtChoose.text!!.length != 13){
+            binding.accountNumberEdtChoose.error = "لطفا یک شماره حساب معتبر وارد کنید"
+            false
+        }else{
+            true
+        }
     }
 }
